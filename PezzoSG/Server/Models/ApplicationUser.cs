eb2db0bb09f66a4305bffd7dcba5f3d32a897228
JAVIER KHOO JUN YING 2003928E <2003928E@student.tp.edu.sg>
2021-12-20 13:55:56 +0800

    First Draft

--- PezzoSG/Server/Models/ApplicationUser.cs
+++ PezzoSG/Server/Models/ApplicationUser.cs
@@ -8,5 +8,7 @@ namespace PezzoSG.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
